db018b975302f999cc9221a2bfaaa969dcb14dcc
Andrii Kohut <kogut.andriy@gmail.com>
2014-05-22 19:22:47 +0300

    implement `winner` method

--- hw1/lib/rock_paper_scissors.rb
+++ hw1/lib/rock_paper_scissors.rb
@@ -4,7 +4,20 @@ class RockPaperScissors
   class NoSuchStrategyError < StandardError ; end
 
   def self.winner(player1, player2)
-    # YOUR CODE HERE
+    rules = {
+      'R' => 'S',
+      'S' => 'P',
+      'P' => 'R'
+    }
+    [player1, player2].each do |player|
+      raise NoSuchStrategyError.new "Strategy must be one of R,P,S" if !rules.keys.include? player[1]
+    end
+    if rules[player1[1]] == player2[1] || player1[1] == player2[1]
+      winner = player1
+    else
+      winner = player2
+    end
+    winner
   end
 
   def self.tournament_winner(tournament)
